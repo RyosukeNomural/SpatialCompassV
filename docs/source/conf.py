37bf0d364c72000e555bb7f6879325112c63f6e6
nomura <nomura@NCCK20250463K.local>
2026-01-09 07:57:40 +0900

    tutorial UI

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,15 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser",]
+extensions = [
+    'myst_parser'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'nbsphinx',
+    'nbgallery',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
